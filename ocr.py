52a6f3d41b2e537ca0972d5d8adc66e3e08ef8be
puranjai <puranjaigarg783@gmail.com>
2024-04-05 22:29:11 -0700

    fix

--- ocr.py
+++ ocr.py
@@ -1,30 +1,10 @@
-import pytesseract
-from PIL import Image
-
-# Load the image
-image_path = '/home/puranjai/projects/usfca/projX/myImage.jpg'
-image = Image.open(image_path)
-
-# Perform OCR
-text = pytesseract.image_to_string(image)
-
-# Print the extracted text
-print(text)
-import pytesseract
-from pytesseract import Output
-from PIL import Image
-import cv2
-
-img_path1 = 'myImage.jpg'
-text = pytesseract.image_to_string(img_path1,lang='eng')
-print(text)
 import easyocr
 
 # Initialize the EasyOCR reader
 reader = easyocr.Reader(['en'], gpu=False)
 
 # Open the image
-image = 'image.jpg'
+image = 'myImage.jpg'
 
 # Use the reader to read text from the image
 results = reader.readtext(image)
@@ -39,7 +19,7 @@ import easyocr
 reader = easyocr.Reader(['en'], gpu=False)
 
 # Open the image
-image = 'image.jpg'
+image = 'myImage.jpg'
 
 # Use the reader to read text from the image
 results = reader.readtext(image)
